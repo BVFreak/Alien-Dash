519555c40f20cf2d9ee9e05369b510dfdc0b2001
BudgieBirb <alfredtassonyi@gmail.com>
2022-09-07 08:25:07 +1000

    new sprite, movement bugs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from operator import truediv
 import pygame
 
 pygame.init()
@@ -6,13 +5,25 @@ pygame.init()
 width, height = 750, 450
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Alien Dash')
+clock = pygame.time.Clock()
 
 # ufo icon
 icon_surface = pygame.image.load('sprites/ufo.png')
 icon = pygame.display.set_icon(icon_surface)
 
-running = True
-while running:
+# player
+player_surface = pygame.image.load('sprites/player.png')
+x_position = 0
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            quit()
+
+    x_position += 1
+    screen.blit(player_surface,(x_position,0))
+
+
+    pygame.display.update()
+    clock.tick(60)
