4ba262d5ed14b06141246a1a7b7321773ac8c01b
ye09 <42485355+BVFreak@users.noreply.github.com>
2022-09-13 13:14:07 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,58 @@ import pygame, sys
 # initialize pygame
 pygame.init()
 
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 screen = pygame.display.set_mode((750, 450))
 pygame.display.set_caption('Alien Dash')
 clock = pygame.time.Clock()
 
-# icon
-icon = pygame.image.load('sprites/ufo.png')
-icon_surface = pygame.display.set_icon(icon)
-
-# ground
-ground_surface = pygame.image.load('sprites/cobblestone.png').convert()
-
-# player
-
-x_increment = 4
-y_increment = 1
-player_surface = pygame.image.load('sprites/player.png').convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (375,386))
-player_gravity = 0
-
-# enemy
-alien_surface = pygame.image.load('sprites/alien.png').convert_alpha()
-alien_rect = alien_surface.get_rect(midbottom = (0,386))
-
 def play():
+  
+  # TEXT
+  textwhendeath_surface = my_font.render('Press return to revive, Escape to quit', False, (255, 255, 255))
+  textwhendeath_rect = textwhendeath_surface.get_rect(center = (750/2,450/3))
+  
+  # icon
+  icon = pygame.image.load('sprites/ufo.png')
+  icon_surface = pygame.display.set_icon(icon)
 
-    running = True
+  # ground
+  ground_surface = pygame.image.load('sprites/cobblestone.png').convert_alpha()
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+  # player
+  x_increment = 4
+  y_increment = 1
+  player_surface = pygame.image.load('sprites/player.png').convert_alpha()
+  player_rect = player_surface.get_rect(midbottom = (375,386))
+  player_gravity = 0
 
+  # enemy
+  alien_surface = pygame.image.load('sprites/alien.png').convert_alpha()
+  alien_rect = alien_surface.get_rect(midbottom = (0,386))
+  
+  running = True
+  notdead = True
+
+
+  while running:
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              pygame.quit()
+              exit()
+    
+      if not notdead:
+        screen.blit(textwhendeath_surface,textwhendeath_rect)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+          play()
+        elif keys[pygame.K_ESCAPE]:
+          pygame.quit()
+          exit()
+
+
+      if notdead:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
             player_rect.x += x_increment
@@ -49,7 +68,6 @@ def play():
     
         screen.blit(ground_surface,(0,386))
 
-
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 386: player_rect.bottom = 386
@@ -59,13 +77,10 @@ def play():
         if alien_rect.x >= 800: alien_rect.x = 0
         screen.blit(alien_surface,alien_rect)
 
-        if alien_rect.colliderect(player_rect):
-            if keys[pygame.K_RETURN]:
-                play()
-            else:
-                running = False
+      if alien_rect.colliderect(player_rect):
+        notdead = False
 
-        pygame.display.update()
-        clock.tick(60)
-    
-    play()
+      pygame.display.update()
+      clock.tick(60)
+
+play()
